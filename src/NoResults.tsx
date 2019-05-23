a49136cccc2022aefe22487679fc5eb2a6ce0b5d
Cole Bemis <colebemis@gmail.com>
2019-05-23 11:56:03 -0700

    Update NoResults component

--- src/NoResults.tsx
+++ src/NoResults.tsx
@@ -1,51 +1,45 @@
-import { Button, constants } from '@ifixit/toolbox';
-import glamorous from 'glamorous';
-import React, { Component } from 'react';
+import { space } from '@core-ds/primitives';
+import { Button } from '@ifixit/toolbox';
+import React from 'react';
+import styled from 'styled-components';
 
-const Container = glamorous('div', {
-   withProps: { role: 'presentation', tabIndex: 0 },
-})({
-   flex: '0 0 100%',
-   padding: constants.spacing[3],
-   textAlign: 'center',
-   display: 'flex',
-   flexDirection: 'column',
-   justifyContent: 'center',
-   alignItems: 'center',
-   maxWidth: '100%',
-});
+const Container = styled.div`
+   flex: 1 1 100%;
+   padding: ${space[4]};
+   display: flex;
+   flex-direction: column;
+   justify-content: center;
+   align-items: center;
+   max-width: 100%;
+   text-align: center;
+`;
 
 interface NoResultsProps {
-   itemName: string;
-   selectItem: (title: string) => void;
+   query: string;
    allowOrphan: boolean;
-   translate: any;
+   selectItem: (title: string) => void;
+   translate: (...strings: string[]) => string;
 }
 
-class NoResults extends Component<NoResultsProps> {
-   render() {
-      const {
-         itemName,
-         selectItem,
-         allowOrphan,
-         translate,
-         ...props
-      } = this.props;
-
-      return (
-         <Container {...props}>
-            <p>{translate('No matches found. Did you spell it correctly?')}</p>
-            {allowOrphan && (
-               <Button
-                  style={{ maxWidth: '100%' }}
-                  onClick={() => selectItem(itemName)}
-               >
-                  {translate('Choose %1', `"${itemName}"`)}
-               </Button>
-            )}
-         </Container>
-      );
-   }
+function NoResults({
+   query,
+   allowOrphan,
+   selectItem,
+   translate,
+}: NoResultsProps) {
+   return (
+      <Container>
+         <p>{translate('No matches found. Did you spell it correctly?')}</p>
+         {allowOrphan ? (
+            <Button
+               style={{ maxWidth: '100%' }}
+               onClick={() => selectItem(query)}
+            >
+               {translate('Choose %1', `"${query}"`)}
+            </Button>
+         ) : null}
+      </Container>
+   );
 }
 
 export default NoResults;
