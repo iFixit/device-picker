75554c30d7b8481e656bb8e88712654fb9cd0d79
Cole Bemis <colebemis@gmail.com>
2019-05-23 15:43:56 -0700

    Refactor indexBy

--- src/utils/indexBy.ts
+++ src/utils/indexBy.ts
@@ -2,10 +2,10 @@ import { Dictionary } from 'lodash';
 
 /** Returns a list indexed by the given key. */
 function indexBy<T>(key: keyof T, list: Array<T>): Dictionary<T> {
-   return list.reduce(
-      (acc, item) => ({ ...acc, [item[key] as any]: item }),
-      {},
-   );
+   return list.reduce((acc: any, item) => {
+      acc[item[key]] = item;
+      return acc;
+   }, {});
 }
 
 export default indexBy;
