45b6f1b0e712d05dd36abc69f2e8c8ba6791025d
Cole Bemis <colebemis@gmail.com>
2019-05-30 16:56:47 -0700

    stringDifference: update description

--- src/utils/stringDifference.ts
+++ src/utils/stringDifference.ts
@@ -1,4 +1,7 @@
-/** Returns a string containing words from stringA that are not in stringB. */
+/**
+ * Returns a string containing words from stringA that are not in stringB
+ * or all of stringA if no such words are present.
+ */
 function stringDifference(stringA: string, stringB: string) {
    const stringBWords = stringB.toLowerCase().split(' ');
    return (
