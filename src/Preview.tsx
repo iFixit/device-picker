82dd697aeb745b593704c99ca34c4d694fe271a5
Cole Bemis <colebemis@gmail.com>
2019-06-04 14:02:07 -0700

    Allow preview to shrink

--- src/Preview.tsx
+++ src/Preview.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-   flex: 1 0 auto;
+   flex: 1 1 auto;
    display: flex;
    flex-direction: column;
    align-items: center;
