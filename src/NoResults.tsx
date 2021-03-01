81558f49daef4d069817c9d6cf724dce5a5e3e76
Ajit Bohra <ajit@lubus.in>
2021-03-01 23:49:41 +0530

    Cleanup NoResults

--- src/NoResults.tsx
+++ src/NoResults.tsx
@@ -3,7 +3,6 @@ import { _js } from '@ifixit/localize';
 import { Button } from '@ifixit/toolbox';
 import React from 'react';
 import styled from 'styled-components';
-import DevicePicker from './DevicePicker';
 
 const Container = styled.div`
    flex: 1 1 100%;
@@ -18,13 +17,13 @@ const Container = styled.div`
 
 interface NoResultsProps {
    query: string;
-   allowOrphan?: boolean;
+   allowOrphan: boolean;
    selectItem: (title: string) => void;
 }
 
 function NoResults({
    query,
-   allowOrphan = DevicePicker.defaultProps.allowOrphan,
+   allowOrphan,
    selectItem,
 }: NoResultsProps) {
    return (
