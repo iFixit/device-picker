47fb5a7302885d7e49fa2b90d649cfa2c4a1948a
emmalopezcode <emma@ifixit.com>
2021-06-07 11:56:02 -0700

    Algolia Device Picker: wire into the system
    Apart from several style changes, the bulk of this commit is adding onClick
    functionality to each hit. This is determined by the leaf data
    structure, if a hit is a leaf, onClick it will select it. Otherwise, it
    resets the context variable of DevicePicker, and renders the
    Hierarchical Device picker at that category. Side note: the
    connectSearchBox and connectHits functions make it possible to use our
    own styled components with their tech.

--- src/AlgoliaDevicePicker.tsx
+++ src/AlgoliaDevicePicker.tsx
@@ -1,13 +1,20 @@
-import * as React from 'react'
-
-import algoliasearch, {SearchClient} from 'algoliasearch/lite';
-import { InstantSearch, SearchBox, connectHits } from 'react-instantsearch-dom';
-import GridItem from "./GridItem"
-import { Grid } from "./GridExplorer"
-
-import { View } from "./HierarchicalDevicePicker"
+import * as React from 'react';
+import { SearchContainer, SearchInput } from './HierarchicalDevicePicker';
+import { Icon } from '@ifixit/toolbox';
+import algoliasearch, { SearchClient } from 'algoliasearch/lite';
+import {
+   InstantSearch,
+   connectHits,
+   connectSearchBox,
+} from 'react-instantsearch-dom';
+import GridItem from './GridItem';
+import { Grid } from './GridExplorer';
+import { View } from './HierarchicalDevicePicker';
+import { color } from '@core-ds/primitives';
 
 interface AlgoliaDevicePickerProps {
+   leaves: object | null;
+   onSelectCategory: (category: string) => void;
    onSubmit: (title: string) => void;
    onCancel: () => void;
    allowOrphan?: boolean;
@@ -15,6 +22,7 @@ interface AlgoliaDevicePickerProps {
    initialView?: View;
    objectName?: string;
    algoliaConfig: AlgoliaConfig;
+   query?: string;
 }
 
 export interface AlgoliaConfig {
@@ -24,42 +32,96 @@ export interface AlgoliaConfig {
 }
 
 export function AlgoliaDevicePicker(props: AlgoliaDevicePickerProps) {
-   const searchClient = algoliasearch(props.algoliaConfig.appId, props.algoliaConfig.apiKey);
+   const searchClient = algoliasearch(
+      props.algoliaConfig.appId,
+      props.algoliaConfig.apiKey,
+   );
 
    return <AlgoliaSearchRoot searchClient={searchClient} {...props} />;
 }
 
-function AlgoliaSearchRoot(props: AlgoliaDevicePickerProps & {searchClient: SearchClient}) {
+function AlgoliaSearchRoot(
+   props: AlgoliaDevicePickerProps & { searchClient: SearchClient },
+) {
    const indexRoot = 'topic';
    const langid = 'en';
-   const indexName = `${props.algoliaConfig.indexPrefix}${indexRoot}_${langid}`
-   return <InstantSearch searchClient={props.searchClient} indexName={indexName}>
-      <SearchBox />
-      <AlgoliaHitsList />
-   </InstantSearch>
+   const indexName = `${props.algoliaConfig.indexPrefix}${indexRoot}_${langid}`;
+   return (
+      <InstantSearch searchClient={props.searchClient} indexName={indexName}>
+         <AlgoliaSearchBox defaultRefinement={props.query} />
+         <AlgoliaHitsList
+            leaves={props.leaves}
+            onSelectCategory={props.onSelectCategory}
+            onSelectLeaf={props.onSubmit}
+         />
+      </InstantSearch>
+   );
 }
 
-function HitsList({ hits }: { hits: DeviceHit[] }) {
-   return <Grid>
-      {hits.map(hit => {
-         return <HitBox hit={hit} />
-      })}
-   </Grid>
+function HitsList({
+   hits,
+   leaves,
+   onSelectCategory,
+   onSelectLeaf,
+}: {
+   hits: any[];
+   leaves: any;
+   onSelectCategory: (category: string) => void;
+   onSelectLeaf: (title: string) => void;
+}) {
+   return (
+      <Grid>
+         {hits.map((hit) => {
+            return (
+               <HitBox
+                  key={hit.title}
+                  hit={hit}
+                  onSelect={
+                     leaves.has(hit.title) ? onSelectLeaf : onSelectCategory
+                  }
+               />
+            );
+         })}
+      </Grid>
+   );
 }
-
-const AlgoliaHitsList = connectHits(HitsList);
 
 interface HitBoxProps {
    hit: DeviceHit;
+   onSelect: (category: string) => void;
 }
 
-function HitBox({hit}: HitBoxProps) {
-   return <GridItem
-      title={hit.title}
-      image={hit.image_url}
-      onClick={() => console.log("Clicked %s", hit.title)}
-   />
+function HitBox({ hit, onSelect }: HitBoxProps) {
+   return (
+      <GridItem
+         title={hit.title}
+         image={hit.image_url}
+         onClick={() => onSelect(hit.title)}
+      />
+   );
 }
+
+function SearchBox({
+   currentRefinement,
+   refine,
+}: {
+   currentRefinement: string;
+   refine: any;
+}) {
+   return (
+      <SearchContainer>
+         <Icon name="search" size={25} color={color.gray5} />
+         <SearchInput
+            autoFocus
+            value={currentRefinement}
+            onChange={(event) => refine(event.target.value)}
+         />
+      </SearchContainer>
+   );
+}
+
+const AlgoliaSearchBox = connectSearchBox(SearchBox);
+const AlgoliaHitsList = connectHits(HitsList);
 
 interface DeviceHit {
    __position: number;
@@ -79,7 +141,7 @@ interface DeviceHit {
    namespace: string;
    objectID: string;
    objectType: string;
-   parts_url: null|string;
+   parts_url: null | string;
    public: number;
    score_device_posts: number;
    score_wiki_views: number;
